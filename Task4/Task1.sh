f136d7ff42a7a158bd4b399370c1d5a37fe2ff3a
DevIGood <71323072+DevIGood@users.noreply.github.com>
2020-11-03 13:09:28 +0200

    Update Task1.sh

--- Task4/Task1.sh
+++ Task4/Task1.sh
@@ -1,1 +1,6 @@
-#temp
+echo "Choose a directory"
+#Examples of input /etc/ /dev/ /lib/ /root/
+#Tho, you are not limited and can write a whole directory if you wish
+read file1
+tree -d $file1
+# tree lists all directories
