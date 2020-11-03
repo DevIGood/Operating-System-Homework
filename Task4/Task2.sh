888b8c166d3b60f7bf71fdcbfa82e77bee693eb3
DevIGood <71323072+DevIGood@users.noreply.github.com>
2020-11-03 13:09:53 +0200

    Update Task2.sh

--- Task4/Task2.sh
+++ Task4/Task2.sh
@@ -1,1 +1,8 @@
-#temp
+echo "Choose a directory"
+#Examples of input /etc/ /dev/ /lib/ /root/ and much more"
+#Tho, you are not limited and can write a whole directory if you wish
+read file1
+find $file1 -printf '%s %p\n' | sort -n | tail -1
+# the find part checks every files in this directory.
+# sort -n just sorts our output numerically
+# and tail -1 outputs us only the last largest file
