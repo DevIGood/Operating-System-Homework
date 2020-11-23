4d85b2b930e47dfc6da1aa90365cba3f94f1d6a1
DevIGood <71323072+DevIGood@users.noreply.github.com>
2020-11-23 22:19:04 +0200

    Update task9.sh

--- Task5/task9.sh
+++ Task5/task9.sh
@@ -1,2 +1,6 @@
 find $HOME -type f -printf '%h\n' | sort | uniq -c | sort -n | tail -n 1
-#work in progress
+#-type f only looks for files
+#-printf '%h\n' finds every file directory
+#then we use sort to make the same directory files be next to eachother
+#uniq -c counts repeating text
+#then this sorts 
